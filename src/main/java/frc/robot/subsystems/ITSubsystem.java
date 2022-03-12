ce45c3edd96ba67709d967f5c429cf73e759db5d
Dhruva Chayapathy <dhruva.chayapathy@gmail.com>
2022-03-12 00:47:38 -0500

    Intake motors added Internal Transport subsystem added Constants changes

---
+++ src/main/java/frc/robot/subsystems/ITSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ITConst;
+public class ITSubsystem extends SubsystemBase{
+	private final CANSparkMax motor1 = new CANSparkMax(ITConst.motor1,MotorType.kBrushless);
+	private final CANSparkMax motor2 = new CANSparkMax(ITConst.motor2,MotorType.kBrushless);
+	public ITSubsystem(){
+		//empty
+	}
+	public void run(){
+		motor1.set(0.5);
+		motor2.set(0.5);
+	}
+	public void stop(){
+		motor1.set(0.0);
+		motor2.set(0.0);
+	}
+}
+
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,23 +1,27 @@
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.SolenoidConstants;
-
-public class IntakeSolenoidSubsystem extends SubsystemBase {
-    
-    // Initialize solenoids
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConst;
+public class intake extends SubsytemBase{
     private final Solenoid leftIntake = new Solenoid(PneumaticsModuleType.REVPH, SolenoidConstants.intakeLeftPort);
     private final Solenoid rightIntake = new Solenoid(PneumaticsModuleType.REVPH, SolenoidConstants.intakeRightPort);
-
+	private final CANSparkMax motor = new CANSparkMax(IntakeConst.IntakeMotor, MotorType.kBrushless);	
     /**
      * Empty constructor to initialize the solenoid subsystem, not necessary
      */
-    public IntakeSolenoidSubsystem() {
+    public intake() {
 
     }
-
+	public void run(){
+		motor.set(0.75);
+	}
+	public void stop(){
+		motor.set(0.0);
+	}
     /**
      * Turns on the left solenoid for the intake.
      */
@@ -31,5 +35,5 @@ public class IntakeSolenoidSubsystem extends SubsystemBase {
     public void dropRightIntake() {
         rightIntake.set(true);
     }
-
+	
 }
